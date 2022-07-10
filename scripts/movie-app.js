cfbcd03d9606ce0b1f77248773a3ddafb4d4df6d
Ed Whyatt <lizardxyz@googlemail.com>
2022-07-10 17:12:50 +0000

    Add get jason to seach information from omdbapi.com

--- scripts/movie-app.js
+++ scripts/movie-app.js
@@ -5,22 +5,41 @@ function fetchMovieInformation() {
 
     // define contence of the serach value
     let search = $("#movie").val();
-    $("movie-data").html("");
+    $("#movie-data").html("");
 
     // if the search box is empty display message
-    if(!search) {
-        $("#loader").html( `<h2 class="search-message text-center">
-                                 Pease Search For A Movie!!</h2>`
-                            );
+    if (!search) {
+        $("#loader").html(`<h2 class="search-message text-center">
+                                 Pease Search For A Movie!!</h2>`);
         return;
     }
     // loader image while searching for a movie
-    $("#loader").html( 
-                            `<div id="book-loader">
+    $("#loader").html(
+            `<div id="book-loader">
                             <img src="assets/loader-image/loader.gif" alt="loading..." />
                             </div>`
-    )
-   
+        ), // gets movie data informaton from Omdbapi.com
+        $.getJSON("https://www.omdbapi.com/?", {
+                apikey: apiKey(),
+                s: search
+            }, function (data) {
+                // filter out undefined data
+                if (data.search !== undefined) {
+                    $.each(data.search, function (index, value) {
+                        if (index < i) {
+                            // search for imdb information from Omdbapi.com
+                            $.getJSON("https://www.omdbapi.com/?", {
+                                    apikey: apiKey(),
+                                    i: value.imdbID
+                                }, function (movieData) {
+                                    (console.log(movieData));
+                                },
+                            )
+                        }
+                    })
+                }
+            }
+        )
 };
 
 
