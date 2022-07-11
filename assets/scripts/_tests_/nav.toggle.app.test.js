02b6d74abc6e160ad6606d3c8ae4ce09d398fb43
Ed Whyatt <lizardxyz@googlemail.com>
2022-07-11 16:46:50 +0000

    Move scripts folder into assets

---
+++ assets/scripts/_tests_/nav.toggle.app.test.js
@@ -0,0 +1,25 @@
+/**
+ * @jest-environment jsdom
+ */
+ const buttonClick = require("../nav-toggle-app2");
+// loads html contence from toggle_app_test.html to mock html page locally for test purposes
+ beforeAll(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("toggle_app_test.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+});
+// Dom test to check the the elements and function
+describe("DOM tests", () => {
+    // test the click function works
+    test("Expects content to change", () => {
+        buttonClick();
+        document.getElementById("nav-list").click()
+        expect(document.getElementById("nav-list").innerHTML).toEqual("You Clicked");
+    });
+    // test the number of div elements on page
+    test("div should exist", () => {
+        expect(document.getElementsByTagName("div").length).toBe(2);
+    })
+});
