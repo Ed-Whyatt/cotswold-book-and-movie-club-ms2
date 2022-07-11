ccd3962449ffbab4cc0bf28f65030487fe79a291
Ed Whyatt <lizardxyz@googlemail.com>
2022-07-11 12:18:35 +0000

    Add initialise and center map function named initmap

---
+++ scripts/maps.js
@@ -0,0 +1,14 @@
+/* jshint esversion: 11, jquery: true */
+
+// all documtation foud at https://developers.google.com/maps/documentation/javascript/marker-clustering?hl=en
+// initialise and center map by getting map id in home index.html page
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 9,
+      center: { lat:  51.820506, lng: -2.000396 },
+    });
+    const infoWindow = new google.maps.InfoWindow({
+      content: "",
+      disableAutoPan: true,
+    });
+}
