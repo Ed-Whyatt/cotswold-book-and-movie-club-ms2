de98139e36137fb092c64b29f74aaf96e47f9cfe
Ed Whyatt <lizardxyz@googlemail.com>
2022-07-12 10:38:46 +0000

    Add get json function that gets search input info from url

--- assets/scripts/book-app.js
+++ assets/scripts/book-app.js
@@ -1,27 +1,33 @@
 /* jshint esversion: 11, jquery: true */
 
-// Fetch the movie search information in the book search bar and search the api for the book
+// Fetch the movie search information in the book search bar and search the api for the book.
 function fetchBookInformation(event) {
 
     // Google book api url from https://developers.google.com/books/docs/v1/using#PerformingSearch 
     let bookUrl = "https://www.googleapis.com/books/v1/volumes?q="
 
-    // define contence of the serach value
+    // Define contence of the serach value'
     let search = $("#book").val();
     $("#book-data").html("");
 
-    // if the search box is empty display message under search bar
+    // If the search box is empty display message under search bar.
     if (!search) {
-        $("#book-data").html(`<h2 class="search-message text-center">
+        $("#loader").html(`<h2 class="search-message text-center">
         Pease Search For A Movie!!</h2>`)
         return;
     }
 
      // loader image gif while searching for a movie shown under search bar from https://icons8.com/preloaders/en/search/ 
-    $("#book-data").html(
+    $("#loader").html(
             `<div id="loader">
                     <img src="assets/loader-image/loader.gif" alt="loading..." />
                     </div>`
+        ),// Gets movie information from google books url.
+        $.getJSON(bookUrl + search,
+            function (bookData) {
+                // (displayBookResults(bookData))
+                console.log(bookData)
+            },
         )
 };
 // function to get the html document ready for the app to start.
